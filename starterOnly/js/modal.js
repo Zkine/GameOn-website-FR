3ae9b4fd592038500a9b3a5087cd6bfc50e01aba
Zkine <philippe_jaymes@hotmail.com>
2023-07-13 06:41:22 +0200

    Ajout du contrôle du formulaire de la modale

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -1,24 +1,180 @@
 // DOM Elements
 const modalbg = document.querySelector(".bground");
-const modalclose = document.querySelector(".close");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-console.log(modalBtn);
+
+//////////////////////////////////ouverture et fermeture de la modale\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 // launch modal form
-function launchModal() {
+function launchModal(e) {
+  const ariaClosest = e.target.closest("main");
+  const closeAria = ariaClosest.querySelector(".close");
   modalbg.style.display = "block";
+  //condition qui permet de basculer de false à true la croix de fermeture de la modale
+  if (!closeAria.hasAttribute("style")) {
+    closeAria.setAttribute("aria-pressed", false);
+  }
 }
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // Fermeture de la modale
 function closeModal(event) {
-  const baliseArticle = event.target.closest("aside");
+  const baliseArticle = event.closest("aside");
   if (baliseArticle.hasAttribute("style")) {
     baliseArticle.removeAttribute("style");
+    event.setAttribute("aria-pressed", true);
     // reset du formulaire après la fermeture
     document.reserve.reset();
   }
 }
-modalclose.addEventListener("click", closeModal);
+
+//////////////////////////////////contrôle du formulaire\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+const regExpNomPrenom = new RegExp("^[a-zA-Z -]{2,}$");
+const regExpEmail = new RegExp(
+  "^[a-z0-9.-_]+[@]{1}[a-z0-9.-_]+[.]{1}[a-z]{2,4}$"
+);
+// vérification du prénom
+const inputPrenom = document.getElementById("first");
+inputPrenom.addEventListener("input", (e) => {
+  e.stopPropagation();
+  const testPrenom = regExpNomPrenom.test(inputPrenom.value);
+  if (testPrenom) {
+    formData[0].dataset.errorVisible = false;
+  } else {
+    formData[0].dataset.errorVisible = true;
+  }
+});
+
+// vérification du nom
+const inputNom = document.getElementById("last");
+inputNom.addEventListener("input", (e) => {
+  e.stopPropagation();
+  const testNom = regExpNomPrenom.test(inputNom.value);
+  if (testNom) {
+    formData[1].dataset.errorVisible = false;
+  } else {
+    formData[1].dataset.errorVisible = true;
+  }
+});
+
+// vérification du email
+const inputMail = document.getElementById("email");
+inputMail.addEventListener("input", (e) => {
+  e.stopPropagation();
+  const testMail = regExpEmail.test(inputMail.value);
+  if (testMail) {
+    formData[2].dataset.errorVisible = false;
+  } else {
+    formData[2].dataset.errorVisible = true;
+  }
+});
+
+// vérification de la date de naissance
+const inputDateNaissance = document.getElementById("birthdate");
+inputDateNaissance.addEventListener(
+  "input",
+  (e) => {
+    e.stopPropagation();
+    const inputAge = inputDateNaissance.value;
+    const testNaissance = inputDateNaissance.reportValidity();
+    const date = new Date(inputAge);
+    const month_diff = Date.now() - date.getTime();
+    const age_dt = new Date(month_diff);
+    var année = age_dt.getUTCFullYear();
+    var age = Math.abs(année - 1970);
+
+    if (testNaissance && age > 13) {
+      formData[3].dataset.errorVisible = false;
+    } else {
+      formData[3].dataset.errorVisible = true;
+      alert("Vous devez avoir plus de 13 ans pour participer aux tournois");
+    }
+  },
+  false
+);
+
+// vérification de la quantité
+const inputQuantity = document.getElementById("quantity");
+inputQuantity.addEventListener("input", (e) => {
+  e.stopPropagation();
+  const testQuantity = inputQuantity.reportValidity();
+  if (testQuantity) {
+    formData[4].dataset.errorVisible = false;
+  } else {
+    formData[4].dataset.errorVisible = true;
+  }
+});
+
+// vérification des conditions générales d'utilisation
+const inputConditionGene = document.getElementById("checkbox1");
+inputConditionGene.addEventListener("change", (e) => {
+  e.stopPropagation();
+  if (e.target.checked) {
+    formData[6].dataset.errorVisible = false;
+  } else {
+    formData[6].dataset.errorVisible = true;
+  }
+});
+
+//////////////////////////////////validation du formulaire\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+function validate(e) {
+  e.preventDefault();
+
+  const testPrenom = regExpNomPrenom.test(inputPrenom.value);
+  const testNom = regExpNomPrenom.test(inputNom.value);
+  const testMail = regExpEmail.test(inputMail.value);
+  const testNaissance = inputDateNaissance.reportValidity();
+  const testQuantity = inputQuantity.reportValidity();
+
+  if (!testPrenom) {
+    formData[0].dataset.errorVisible = true;
+  } else if (testPrenom) {
+    formData[0].dataset.errorVisible = false;
+  }
+
+  if (!testNom) {
+    formData[1].dataset.errorVisible = true;
+  } else if (testNom) {
+    formData[1].dataset.errorVisible = false;
+  }
+
+  if (!testMail) {
+    formData[2].dataset.errorVisible = true;
+  } else if (testMail) {
+    formData[2].dataset.errorVisible = false;
+  }
+
+  if (!testNaissance) {
+    formData[3].dataset.errorVisible = true;
+  } else if (testNaissance) {
+    formData[3].dataset.errorVisible = false;
+  }
+
+  if (!testQuantity) {
+    formData[4].dataset.errorVisible = true;
+  } else if (testQuantity) {
+    formData[4].dataset.errorVisible = false;
+  }
+
+  if (!inputConditionGene.checked) {
+    formData[6].dataset.errorVisible = true;
+  } else if (inputConditionGene.checked) {
+    formData[6].dataset.errorVisible = false;
+  }
+
+  if (
+    testPrenom &&
+    testNom &&
+    testMail &&
+    testNaissance &&
+    testQuantity &&
+    inputConditionGene.checked
+  ) {
+    document.querySelector(
+      ".modal-body"
+    ).innerHTML = `<div class="conteneur-inscription"><p class="titre-inscription">Merci pour </br> votre inscription</p></div>
+    <button class="btn-submit" onclick="closeModal(this)">Fermer</button>`;
+  }
+}
--- starterOnly/js/topnav.js
+++ starterOnly/js/topnav.js
@@ -1,13 +1,10 @@
-// Element du DOM
-const menu = document.getElementById("menu");
-
 // function qui permet de faire apparaitre la Topnav en FirstMobile
 function editNav(e) {
   const topNav = document.getElementById("myTopnav");
-  const iconActive = e.target;
+  const iconActive = e;
   if (topNav.className === "topnav") {
     topNav.className += "responsive";
-    // ajout de la active à la class icon
+    // ajout le mot active à la suite de class icon
     iconActive.className += "active";
     // methode qui permet à l'attribut aria-pressed d'être true lorsque la barre nav est apparente
     iconActive.setAttribute("aria-pressed", true);
@@ -17,4 +14,3 @@ function editNav(e) {
     iconActive.setAttribute("aria-pressed", false);
   }
 }
-menu.addEventListener("click", editNav);
